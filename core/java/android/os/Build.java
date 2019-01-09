rashedsahaji/frameworks_base

Build: Add OS display version key
Signed-off-by: Mohan Cm <mohanmanjappa@gmail.com>

core/java/android/os/Build.java
@@ -47,6 +47,11 @@ public class Build {
     /** A build ID string meant for displaying to the user */
     public static final String DISPLAY = getString("ro.build.display.id");
 
+    /**
+     * @hide
+     */
+    public static final String SUPERIOR_DISPLAY_VERSION = getString("ro.superior.display.version");
+
     /** The name of the overall product. */
     public static final String PRODUCT = getString("ro.product.name");
 

beingmishra/platform_frameworks_base

core/java/android/os/Build.java
@@ -47,6 +47,11 @@ public class Build {
     /** A build ID string meant for displaying to the user */
     public static final String DISPLAY = getString("ro.build.display.id");
 
+    /**
+     * @hide
+     */
+    public static final String PEARL_DISPLAY_VERSION = getString("ro.pearl.display.version");
+
     /** The name of the overall product. */
     public static final String PRODUCT = getString("ro.product.name");
 