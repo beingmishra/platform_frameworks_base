b25b00b054f1da3838fb3d5211c932a13228f39b
PMS22 <prathams99@rediff.com>
2019-03-21 14:06:36 +0000

    SystemUI: Add Gaming mode tile & Create SystemSettings.java
    SystemSetting is authored by mydongistiny<jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -35,6 +35,7 @@ import com.android.systemui.qs.tiles.CPUInfoTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.qs.tiles.GamingModeTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
@@ -116,6 +117,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new RebootTile(mHost);
             case "heads_up":
                 return new HeadsUpTile(mHost);
+            case "gaming":
+                return new GamingModeTile(mHost);
         }
 
         // Intent tiles.
