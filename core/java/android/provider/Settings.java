aeb81ffc984e28af4f7ed3136baa5b10d29419cd
xyyx <xyyx@mail.ru>
2019-04-03 08:27:04 +0000

    Unlock keystore with fingerprint after reboot (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4545,6 +4545,12 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_CLOCK_SELECTION = "lockscreen_clock_selection";
 
+       /**
+	 * Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
